497e9b7bec3108eeeace5dc30555bd8ee46aec08
hoangdinhthien <111034450+hoangdinhthien@users.noreply.github.com>
2025-12-24 22:56:03 +0700

    update refund

--- components/manager/create-order-modal.tsx
+++ components/manager/create-order-modal.tsx
@@ -704,6 +704,12 @@ export function CreateOrderModal({
                             <span>Đã hết hạn</span>
                           </div>
                         </SelectItem>
+                        <SelectItem value='refunded'>
+                          <div className='flex items-center gap-2'>
+                            <div className='w-2 h-2 rounded-full bg-orange-500'></div>
+                            <span>Đã hoàn tiền</span>
+                          </div>
+                        </SelectItem>
                       </SelectContent>
                     </Select>
                   </div>
